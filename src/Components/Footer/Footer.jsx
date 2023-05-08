32f8c743c1be6b02b0d6c9789ed476b9b7c6c6dd
ezequieliribarren <ezequielgiribarren@gmail.com>
2023-05-08 10:06:26 -0300

    footer mantenimiento

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,25 +10,33 @@ const Footer = () => {
             <a href="#nosotros">Nosotros</a>
             </div>
 
+            <div className='imgFoot'>
+                  <a href="#"><img src="/images/vectores/versaFoot.png" alt="Versa" /></a>
+                  </div>
+
         <div className='redesFoot'>
           <div className='redes'>
-                <div><a href="https://www.instagram.com/versaalodigital/">@versadigital</a><a href="https://www.instagram.com/versaalodigital/"><img src="/images/vectores/inst.png" alt="Instagram" />
-            </a></div> <div><a href="https://www.facebook.com/profile.php?id=100092003971412"><img src="/images/vectores/face.png" alt="Facebook" /></a> <a href="https://www.facebook.com/profile.php?id=100092003971412">versadigital</a> </div> 
+                <div>
+                  <a href="https://www.instagram.com/versaalodigital/">@versadigital</a><a href="https://www.instagram.com/versaalodigital/"><img src="/images/vectores/inst.png" alt="Instagram" /></a>
+                </div>    
+            
+            <div>
+              <a href="https://www.facebook.com/profile.php?id=100092003971412"><img src="/images/vectores/face.png" alt="Facebook" /></a> <a href="https://www.facebook.com/profile.php?id=100092003971412">versadigital</a> 
+            </div> 
           </div>
        
-
             </div>
-      </div>
 
-      <div className='imgFoot'> 
-            <a href="#"><img src="/images/vectores/versaFoot.png" alt="Versa" /></a>
+              <div className='copyRight'> 
+    
             <div className='copyVersa'>
             <p>&copy; [2023] Versa Digital. Todos los derechos reservados.</p>
             <a target='_blank' href="www.freepik.com"> <p>Algunas imágenes utilizadas en este sitio web son de Freepik y están sujetas a sus términos de uso y atribución</p></a>
             </div> 
             </div>
+      </div>
 
-
+    
 
     </footer>
   )
