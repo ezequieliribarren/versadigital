6db133cdacaf46a10b7a719ebfd84e5172d26f17
ezequieliribarren <ezequielgiribarren@gmail.com>
2023-05-26 17:18:56 -0300

    preloader

--- src/App.js
+++ src/App.js
@@ -3,16 +3,35 @@ import Main from './Components/Main/Main';
 import Navbar from './Components/Navbar/Navbar';
 import Contacto from './Components/Contacto/Contacto';
 import Footer from './Components/Footer/Footer';
+import { useEffect, useState } from 'react';
+import { SquareLoader } from 'react-spinners';
+
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.onload = () => {
+      setLoading(false);
+    };
+  }, []);
   return (
-    <div>
-  <Navbar/>
-  <Main/>
-  <div className='break'> 
-        <img src="/images/nosotros/break.png" alt="" /></div>
-  <Contacto/>
-  <Footer/>
+    <div >
+      {loading ? (
+        <div className='preload'> 
+        <SquareLoader color="rgb(255, 255, 255)" size={80}/></div>
+      ) : (
+        <div>
+          <Navbar />
+          <Main />
+          <div className='break'>
+            <img src="/images/nosotros/break.png" alt="" />
+          </div>
+          <Contacto />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
